6d1b22524078c858683126e4885f922842831a31
Un1R <132201416+Un1R@users.noreply.github.com>
2023-05-24 07:36:59 -0700

    Create ShopPrices.lua

---
+++ ShopPrices.lua
@@ -0,0 +1,8 @@
+local module = {
+	["Fists"] = {
+		Price = 0;
+		ToolPath = game:GetService("ServerScriptService"):FindFirstChild("Tools"):FindFirstChild("Container"):FindFirstChild("Fists");
+		Currency = "Gold";
+	}
+}
+return module
